d674e849d4d753234a865ab46bad0273c9ee3e65
Fathy Sherif <116979040+fathysherif0@users.noreply.github.com>
2022-12-25 10:33:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const submitButton = document.getElementById('submit-button');
+const messageDiv = document.getElementById('message');
+
+submitButton.addEventListener('click', function() {
+  // Generate random reservation number
+  const reservationNumber = Math.floor(Math.random() * 1000000);
+
+  // Display message and reservation number
+  messageDiv.textContent = `Reservation successful!
+  Thank you for using MRWASH
+  Market One of our employees will contact you and confirm the reservation and payment, and this  ${reservationNumber} password for you to enter MR WASH will be effective when confirming the payment amount .`;
+
+  // Get form data
+  const formData = new FormData(document.getElementById('booking-form'));
+
+  // Convert form data to array of values
+  const data = [];
+  formData.forEach((value, key) => {
+    data.push(value);
+  });
+
+  // Append form data to Google Sheet
+  gapi.client.sheets.spreadsheets.values.append({
+    spreadsheetId: 'project-189890741136',
+    range: 'Sheet1',
+    valueInputOption: 'USER_ENTERED',
+    insertDataOption: 'INSERT_ROWS',
+    resource: {
+      values: [data]
+    }
+  }).then((response) => {
+    console.log(response);
+  });
+});
